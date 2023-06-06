f2f00f5b5537b719f425ba55845be12b0e635ee9
Korchiko <ismailldmirrr@gmail.com>
2023-06-06 23:38:01 +0300

    emin

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-red-400 bg-fuchsia-500'>Bay MRRRR</h1>
+      <h1 className='text-red-400 bg-fuchsia-500'>Bay Pala</h1>
     </div>
   );
 }

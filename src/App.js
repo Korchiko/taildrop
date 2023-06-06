8252f0bd4cf841da77ece53f90d64886c9c1b68a
Korchiko <ismailldmirrr@gmail.com>
2023-06-06 14:11:10 +0300

    Halledildi

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-red-400 bg-fuchsia-500'>Bay MRRRR</h1>
+    </div>
+  );
+}
+
+export default App;

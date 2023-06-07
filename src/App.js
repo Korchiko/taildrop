cbf199e84917f9c46feb9a81a1c1556a574be254
Korchiko <ismailldmirrr@gmail.com>
2023-06-07 03:08:08 +0300

    son update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 
 import './App.css';
+import Footer from './Components/Footer';
 import Header from './Components/Header'
+import Content from './Components/Content'
+import Profile from './Components/Profile'
+import Picture from './Components/Picture'
+
 
 function App() {
   return (
     <div>
     <Header/>
+    <Content/>
+    <Picture/>
+    <Profile/>
+    <Footer/>
+  
     </div>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,14 +9,14 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
  
 function NavList() {
   return (
-    <ul className="my-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+    <ul className="my-2 flex flex-col gap-5 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-8">
       <Typography
         as="li"
         variant="medium"
         color="blue-gray"
-        className="p-1 font-medium"
+        className="p-5 font-medium"
       >
-        <a href="#" className="flex items-center hover:text-blue-500 transition-colors">
+        <a href="#" className=" flex items-center hover:text-blue-500 transition-colors">
           Ana Sayfa
         </a>
       </Typography>
